1568d25b7e41a2f29a2f8a97eda123585c7a2558
wisehead <alex.chenhui@gmail.com>
2017-10-23 10:18:29 +0800

    007.Copy_List

---
+++ 007.Copy_List/007.copy_list.py
@@ -0,0 +1,18 @@
+#############################################################
+#   File Name: 007.copy_list.py
+#     Autohor: Hui Chen (c) 2017
+#        Mail: chenhui13@baidu.com
+# Create Time: 2017/10/23-10:14:58
+#############################################################
+#!/usr/bin/env python 
+#-*- coding:utf8 -*-
+
+list1 = ['physics', 'chemistry', 1997, 2000];
+list2 = [1, 2, 3, 4, 5, 6, 7 ];
+
+list3 = list1[:]
+print list1
+print list3
+list4 = list2[:]
+print list2
+print list4
