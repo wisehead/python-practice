24aad46513bc44a26812af445d866de13109dff6
wisehead <alex.chenhui@gmail.com>
2017-10-23 10:43:37 +0800

    009.Sleep

---
+++ 009.Sleep/009.sleep.py
@@ -0,0 +1,12 @@
+#############################################################
+#   File Name: 009.sleep.py
+#     Autohor: Hui Chen (c) 2017
+#        Mail: chenhui13@baidu.com
+# Create Time: 2017/10/23-10:39:30
+#############################################################
+#!/usr/bin/env python 
+#-*- coding:utf8 -*-
+from time import sleep
+print "before sleep."
+sleep(1)
+print "after sleep."
