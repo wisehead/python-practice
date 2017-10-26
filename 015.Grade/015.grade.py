7b0d4fcd868452a2fbe050706703167efb4d4b14
wisehead <alex.chenhui@gmail.com>
2017-10-26 15:28:17 +0800

    015.Grade

---
+++ 015.Grade/015.grade.py
@@ -0,0 +1,15 @@
+#############################################################
+#   File Name: 015.grade.py
+#     Autohor: Hui Chen (c) 2017
+#        Mail: chenhui13@baidu.com
+# Create Time: 2017/10/26-15:18:20
+#############################################################
+#!/usr/bin/env python 
+#-*- coding:utf8 -*-
+grade = long(raw_input("please input grade: "))
+if grade >= 90:
+	print 'A'
+elif grade >=60:
+	print 'B'
+else:
+	print 'C'
